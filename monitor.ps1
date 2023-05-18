a11f944b9e725bf47f7d8f9d315a7d45b99235b9
panchakarlabk <bharat_icom@icomsol.co.uk>
2023-05-18 09:49:58 +0100

    first commit

---
+++ monitor.ps1
@@ -0,0 +1,21 @@
+$intervalProcesses = 5  # Interval to monitor processes in seconds
+$intervalFolder = 10    # Interval to monitor folder contents in seconds
+$folderPath = "C:/Users/sorint/Desktop/code/Transmogrified"
+
+function MonitorProcesses {
+    Write-Host "Running processes:"
+    Get-Process | Select-Object Id, ProcessName, UserName | Format-Table -AutoSize
+}
+
+function MonitorFolder {
+    Write-Host "Folder contents:"
+    Get-ChildItem $folderPath | Select-Object Name, LastWriteTime | Format-Table -AutoSize
+}
+
+while ($true) {
+    MonitorProcesses
+    Start-Sleep -Seconds $intervalProcesses
+
+    MonitorFolder
+    Start-Sleep -Seconds $intervalFolder
+}
